c8fe19915905c8473add6dbee025e0d92551c7b4
Rian Corcino <riancorci@gmail.com>
2023-11-08 21:24:07 -0800

    added multi screen activities

---
+++ app/src/main/java/com/example/ai_flashcards/StudyActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ai_flashcards
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StudyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_study)
+    }
+}

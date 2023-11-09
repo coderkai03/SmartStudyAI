acb0c9ca745a2d82fa5a7d81e383b910ebfd09c5
grumblefoot <70971715+grumblefoot@users.noreply.github.com>
2023-11-08 19:54:31 -0800

    Debug main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.ai_flashcards"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.ai_flashcards"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 

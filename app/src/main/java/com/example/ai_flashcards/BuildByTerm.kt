7838900a49017f377a3ac7b7a47a8444238f83bf
Rian Corcino <riancorci@gmail.com>
2023-11-13 21:26:57 -0800

    integrated ChatGPT API

--- app/src/main/java/com/example/ai_flashcards/BuildByTerm.kt
+++ app/src/main/java/com/example/ai_flashcards/BuildByTerm.kt
@@ -7,25 +7,44 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import android.os.AsyncTask
+import android.view.View
+import android.widget.TextView
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.DataOutputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.concurrent.CountDownLatch
+
+interface OnApiDataReceivedListener {
+    fun onApiDataReceived(data: String)
+}
+
 
 class BuildByTerm : AppCompatActivity() {
-
+    private val openaiApiKey = "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"
+    private lateinit var sharedPrefs: SharedPreferences
+    private lateinit var inputTerm: EditText
+    private lateinit var loading: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_build_by_term)
 
+        loading = findViewById(R.id.loading_term)
+        loading.visibility = View.INVISIBLE
+
         //sharedPreferences
-        val sharedPrefs: SharedPreferences
         sharedPrefs = this.getSharedPreferences(this.packageName, Context.MODE_PRIVATE)
 
         //buttons
         val topic_screen: Button
         val study_screen: Button
 
-        val inputTerm = findViewById<EditText>(R.id.input_term)
+        inputTerm = findViewById<EditText>(R.id.input_term)
 
         //assign buttons
         topic_screen = findViewById(R.id.topic_button)
@@ -38,27 +57,123 @@ class BuildByTerm : AppCompatActivity() {
             startActivity(intent)
         }
 
-        // Add_button add clicklistener
         study_screen.setOnClickListener {
+            loading.visibility = View.VISIBLE
+
             val intent = Intent(this, StudyActivity::class.java)
+            val apiTask = ApiTask(object : OnApiDataReceivedListener {
+                override fun onApiDataReceived(data: String) {
+                    // This callback is triggered when JSON data is received
+                    Log.d("ByTerm", "Received API data: $data")
 
-            val spsize = sharedPrefs.all.size
-            val cardIndex = if (sharedPrefs.contains("input_topic")) spsize else spsize+1
-            with(sharedPrefs.edit()) {
+                    // Process the data or update shared preferences as needed
+                    val spsize = sharedPrefs.all.size
+                    val cardIndex = if (sharedPrefs.contains("input_topic")) spsize else spsize + 1
 
-                putString("card${cardIndex}", inputTerm.text.toString()+"|null${cardIndex}")
+                    val content = parseJsonResponse(data)
 
-                apply()
+                    with(sharedPrefs.edit()) {
+                        putString("card$cardIndex", inputTerm.text.toString() + "|" + content)
+                        apply()
+                    }
+
+                    val spterm = sharedPrefs.getString("card$cardIndex", "not found")
+                    val term = if (spterm == null) "not found" else spterm
+                    Log.d("ByTerm", "SHAREDPREFS: ${sharedPrefs.all}")
+
+
+                    Log.d("ByTerm","Building...")
+
+                    // start the activity connect to the specified class
+                    startActivity(intent)
+                }
+            })
+
+            apiTask.execute()
+
+
+        }
+    }
+
+    private fun parseJsonResponse(data: String): String {
+        try {
+            val jsonObject = JSONObject(data)
+            val choicesArray = jsonObject.getJSONArray("choices")
+
+            if (choicesArray.length() > 0) {
+                val firstChoice = choicesArray.getJSONObject(0)
+                val message = firstChoice.getJSONObject("message")
+                return message.getString("content")
             }
+        } catch (e: Exception) {
+            Log.e("ByTerm", "Error parsing JSON: ${e.message}")
+        }
 
-            val spterm = sharedPrefs.getString("card${cardIndex}", "not found")
+        return "Error parsing JSON"
+    }
 
-            val term = if (spterm == null) "not found" else spterm
+    private inner class ApiTask(private val onDataReceivedListener: OnApiDataReceivedListener) : AsyncTask<Void, Void, String>() {
+        private lateinit var countDownLatch: CountDownLatch
 
-            Log.d("ByTerm", term)
+        override fun doInBackground(vararg params: Void?): String {
+            return makeApiCall()
+        }
 
-            // start the activity connect to the specified class
-            startActivity(intent)
+        override fun onPostExecute(result: String) {
+            // Handle the API response here
+            Log.d("API_RESPONSE", result)
+            onDataReceivedListener.onApiDataReceived(result)
+            //responseTextView.text = result
+
+        }
+
+        private fun makeApiCall(): String {
+            val apiUrl = "https://api.openai.com/v1/chat/completions"
+            val url = URL(apiUrl)
+            val connection = url.openConnection() as HttpURLConnection
+
+            try {
+                connection.requestMethod = "POST"
+                connection.setRequestProperty("Content-Type", "application/json")
+                connection.setRequestProperty("Authorization", "Bearer $openaiApiKey")
+                connection.doOutput = true
+
+                val data = """
+                    {
+                        "model": "gpt-3.5-turbo",
+                        "messages": [
+                            {
+                                "role": "user",
+                                "content": "Generate a short definition for ${inputTerm.text.toString()}. Limit 1 sentence."
+                            }
+                        ]
+                    }
+                """.trimIndent()
+
+                Log.d("ByTerm", data)
+
+                val outputStream = DataOutputStream(connection.outputStream)
+                outputStream.writeBytes(data)
+                outputStream.flush()
+                outputStream.close()
+
+                val responseCode = connection.responseCode
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    val reader = BufferedReader(InputStreamReader(connection.inputStream))
+                    val response = StringBuilder()
+                    var line: String?
+                    while (reader.readLine().also { line = it } != null) {
+                        response.append(line)
+                    }
+                    reader.close()
+                    return response.toString()
+                } else {
+                    return "Error: $responseCode"
+                }
+            } finally {
+                Log.d("ByTerm", "API")
+                connection.disconnect()
+            }
         }
     }
 }
